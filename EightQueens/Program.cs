ccfb673c9d3943fd1e9ab633d4dd51d996fa20f9
Marcin Hoppe <marcin.hoppe@gmail.com>
2014-03-28 11:37:00 +0100

    Add source code

---
+++ EightQueens/Program.cs
@@ -0,0 +1,16 @@
+﻿// Author: Marcin Hoppe <marcin.hoppe (at) gmail.com>
+
+using System;
+
+namespace MarcinHoppe.EightQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem = new EightQueensProblem();
+            problem.Solve();
+            problem.PrintSolution();
+        }
+    }
+}
